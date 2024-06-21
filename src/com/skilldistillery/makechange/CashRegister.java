0c7da84961b7acf24926948cd4b6a9f5a8dc790f
Rob Roselius <roselius@skilldistillery.com>
2024-06-21 17:15:08 -0600

    Create and close Scanner in main

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -1,11 +1,18 @@
 package com.skilldistillery.makechange;
 
+import java.util.Scanner;
+
 public class CashRegister {
 
 	public static void main(String[] args) {
 
 		//TODO - create scanner
 		//TODO - prompt for inputs
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		
+		keyboard.close();
 	}
 
 }
