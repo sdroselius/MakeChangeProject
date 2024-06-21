b0296cc7895b0f34abc533312e216137a19489f7
Rob Roselius <roselius@skilldistillery.com>
2024-06-21 17:18:08 -0600

    Prompt for price

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -6,11 +6,13 @@ public class CashRegister {
 
 	public static void main(String[] args) {
 
-		//TODO - create scanner
 		//TODO - prompt for inputs
 		
 		Scanner keyboard = new Scanner(System.in);
 		
+		System.out.print("Enter the price: ");
+		double price = keyboard.nextDouble();
+		System.out.println("Price: " + price);
 		
 		keyboard.close();
 	}
